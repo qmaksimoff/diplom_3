9c1c380e92b9e3155b6d1eb358ed96c3c4e6559a
Aleksandr Maksimov <qmaksimoff@gmail.com>
2022-11-18 21:08:49 +0300

    Done. Only Google Chrome

---
+++ src/main/java/pageobject/ForgotPasswordPage.java
@@ -0,0 +1,32 @@
+package pageobject;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ForgotPasswordPage extends BasicPages{
+    public ForgotPasswordPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = ".//button[text()='Восстановить пароль']")
+    private WebElement recoverPasswordButton;
+    @FindBy(linkText = "Войти")
+    private WebElement loginLink;
+
+    @Step("Ожидание загрузки страницы")
+    public ForgotPasswordPage waitForPage() {
+        waitForElement(recoverPasswordButton);
+        return this;
+    }
+
+    @Step("Переход на страницу логина")
+    public LoginPage goToLoginPage() {
+        waitForPage();
+        wait(10);
+        loginLink.click();
+        return new LoginPage(driver);
+    }
+
+}
